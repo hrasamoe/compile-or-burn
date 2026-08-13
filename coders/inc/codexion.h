1de1237943f5115836c5ac5df47c939aba2ee578
hrasamoe <herysamuelpljv@gmail.com>
2026-08-13 10:50:39 +0300

    Update codexion.h with new coder state enum and struct definitions

--- coders/inc/codexion.h
+++ coders/inc/codexion.h
@@ -6,7 +6,7 @@
 /*   By: hrasamoe <hrasamoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 14:07:23 by hrasamoe          #+#    #+#             */
-/*   Updated: 2026/08/13 10:49:09 by hrasamoe         ###   ########.fr       */
+/*   Updated: 2026/08/13 10:50:31 by hrasamoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,20 @@ typedef enum e_schedule
 	EDF
 }	t_schedule;
 
+typedef enum e_coder_state
+{
+	THINKING,
+	DEBUGGING,
+	COMPILING,
+	BURNED_OUT,
+	REFACTORING,
+	WAITING_DONGLES
+}	t_coder_state;
 
+typedef struct s_simulator		t_simulator;
+typedef struct s_heap			t_heap;
+typedef struct s_coder			t_coder;
+typedef struct s_dongle			t_dongle;
+typedef struct s_request		t_request;
 
 #endif
