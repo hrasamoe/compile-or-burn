476ae63e4a17b42fd89e960ea6f58fdde2395428
hrasamoe <herysamuelpljv@gmail.com>
2026-08-25 14:04:27 +0300

    Add utils.c for time management and dongle readiness checks

--- coders/tools/utils.c
+++ coders/tools/utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   time.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hrasamoe <hrasamoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 13:38:48 by hrasamoe          #+#    #+#             */
-/*   Updated: 2026/08/13 14:13:31 by hrasamoe         ###   ########.fr       */
+/*   Updated: 2026/08/25 14:03:52 by hrasamoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,19 @@ long long	get_current_time(void)
 	if (gettimeofday(&tv, NULL) == -1)
 		return (0);
 	return ((tv.tv_sec * 1000LL) + (tv.tv_usec / 1000LL));
+}
+
+static int	is_dongle_ready(t_dongle *dongle)
+{
+	long	now;
+
+	now = get_current_time();
+	return (dongle->is_available && now > dongle->unvailable_until);
+}
+
+int	are_dongles_ready(t_dongle *dongle_left, t_dongle *dongle_right)
+{
+	if (dongle_left == dongle_right)
+		return (is_dongle_ready(dongle_left));
+	return (is_dongle_ready(dongle_left) && is_dongle_ready(dongle_right));
 }
