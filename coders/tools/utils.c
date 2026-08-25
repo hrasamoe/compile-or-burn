8c09b4d8f89dba2e63e6eb3486ccb6fef9c6ce38
hrasamoe <herysamuelpljv@gmail.com>
2026-08-25 16:16:58 +0300

    Add simulator.c for simulation control and update utils.c with stop flag and precise sleep functions

--- coders/tools/utils.c
+++ coders/tools/utils.c
@@ -6,11 +6,18 @@
 /*   By: hrasamoe <hrasamoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 13:38:48 by hrasamoe          #+#    #+#             */
-/*   Updated: 2026/08/25 14:03:52 by hrasamoe         ###   ########.fr       */
+/*   Updated: 2026/08/25 15:20:40 by hrasamoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/codexion.h"
+
+void	set_stop_flag(t_simulator *simulation)
+{
+	pthread_mutex_lock(&simulation->stop_lock);
+	simulation->stop = 1;
+	pthread_mutex_lock(&simulation->stop_lock);
+}
 
 long long	get_current_time(void)
 {
@@ -34,4 +41,17 @@ int	are_dongles_ready(t_dongle *dongle_left, t_dongle *dongle_right)
 	if (dongle_left == dongle_right)
 		return (is_dongle_ready(dongle_left));
 	return (is_dongle_ready(dongle_left) && is_dongle_ready(dongle_right));
+}
+
+void	precise_sleep(t_simulator *simulation, long duration)
+{
+	long	start;
+
+	start = get_current_time();
+	while (!should_stop(simulation))
+	{
+		if (get_current_time() - start >= duration)
+			break ;
+		usleep(500);
+	}
 }
