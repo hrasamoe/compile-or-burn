e8e9cd93209b1445d831bf64325cba676bb3612e
hrasamoe <herysamuelpljv@gmail.com>
2026-08-26 13:48:25 +0300

    Update coder_action.c: correct update timestamp and fix mutex locking in coder_compile

--- coders/tools/coder_action.c
+++ coders/tools/coder_action.c
@@ -6,7 +6,7 @@
 /*   By: hrasamoe <hrasamoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 14:22:26 by hrasamoe          #+#    #+#             */
-/*   Updated: 2026/08/25 15:09:48 by hrasamoe         ###   ########.fr       */
+/*   Updated: 2026/08/26 13:34:27 by hrasamoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ void	coder_compile(t_coder *coder)
 	if (should_stop(coder->simulator))
 		return ;
 	pthread_mutex_lock(&coder->lock);
+	print_log(coder, "is compiling");
 	coder->last_compilation = get_current_time();
 	coder->state = COMPILING;
 	precise_sleep(coder->simulator, coder->simulator->time_to_compile);
-	pthread_mutex_lock(&coder->lock);
 	coder->nb_compilation++;
 	pthread_mutex_unlock(&coder->lock);
 }
