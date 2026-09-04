2d7480384e45be4059ac7676c066db60d02b1c45
hrasamoe <herysamuelpljv@gmail.com>
2026-09-04 14:20:45 +0300

    Fix typo in variable name for time_to_refactor in parsing.c

--- coders/tools/parsing.c
+++ coders/tools/parsing.c
@@ -6,7 +6,7 @@
 /*   By: hrasamoe <hrasamoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 13:58:19 by hrasamoe          #+#    #+#             */
-/*   Updated: 2026/08/30 08:16:02 by hrasamoe         ###   ########.fr       */
+/*   Updated: 2026/09/04 13:54:40 by hrasamoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,7 @@ int	parse_arguments(int argc, char **argv, t_simulator *sim)
 	sim->time_to_burnout = atoi(argv[2]);
 	sim->time_to_compile = atoi(argv[3]);
 	sim->time_to_debug = atoi(argv[4]);
-	sim->time_to_refrator = atoi(argv[5]);
+	sim->time_to_refactor = atoi(argv[5]);
 	sim->nb_compilation_required = atoi(argv[6]);
 	sim->dongle_cooldown = atoi(argv[7]);
 	if (strcmp(argv[8], "fifo") == 0)
